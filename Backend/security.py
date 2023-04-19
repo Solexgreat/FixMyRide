ca65029a60e23efc3c135a8b03b5a6e0aae3ce03
Solexgreat <solomonGodkid@gmail.com>
2023-04-19 23:46:47 +0100

    Secrity

--- Backend/security.py
+++ Backend/security.py
@@ -1,6 +1,1 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore
-from db import DB
 
-user_datastore = SQLAlchemyUserDatastore()
