20a9ab0e9d1eeb52d1018fb5d9c83bb555962ab3
Ehigiepaul01 <paulehigie5@gmail.com>
2023-04-21 11:29:19 +0100

    updated the codes

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger-menu");
+const navList = document.querySelector(".nav-list");
+hamburger.addEventListener("click", ()=> {
+    hamburger.classList.toggle("active")
+    navList.classList.toggle("show")
+
+    
+})
